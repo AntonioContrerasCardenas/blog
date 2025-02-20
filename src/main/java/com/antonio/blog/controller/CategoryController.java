eb41b81ef416ef08dc88ed2a409385e729ab327b
Antonio Contreras <aconcar181@g.educaand.es>
2025-02-20 13:41:26 +0100

    Add authorization to delete category endpoint: restrict access to users with ADMIN role

--- src/main/java/com/antonio/blog/controller/CategoryController.java
+++ src/main/java/com/antonio/blog/controller/CategoryController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,6 +38,7 @@ public class CategoryController {
         return new ResponseEntity<>(category, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> deleteCategory(@PathVariable("id") Long id) {
         this.categoryService.deleteCategory(id);
